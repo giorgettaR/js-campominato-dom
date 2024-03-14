01aeffd27d33eff9f0cc8ca6ce4b42f5d840b504
GiorgettaR <giorgetta.roberto@gmail.com>
2024-03-14 17:40:32 +0100

    FIX: html/css - style / js - functions

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-// Consegna
-// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell’esercizio ma solo l’index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l’inizializzazione di git).
-// Descrizione:
-
-// In seguito l’utente clicca su una cella:
-// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso.
-// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
-// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-// Attenzione che l’utente potrebbe cliccare due volte sulla stessa casella…
-// BONUS 2:
-// Quando al partita termina mostrare nella griglia tutte le bombe presenti, anche quelle che non erano state trovate.
-// Tutte le caselle delle bombe devono diventare rosse
-// BONUS 3:
-// Una volta che la partita termina l’utente non deve più poter cliccare sulle cella, nel senso che se anche ci clicca non deve succedere niente.
-
 
 const gridEl = document.querySelector('.container');
 const playButtonEl = document.getElementById('playButton');
@@ -37,40 +21,32 @@ playButtonEl.addEventListener('click', function(){
         numOfCells = 49;
     }
 
-    grid(numOfCells, difficulty);
+    startGame(numOfCells, difficulty);
 })
 
 // funzione per generare griglia
 
-function grid(numOfCells, difficulty){
+function startGame(numOfCells, difficulty){
     
     let points = 0;
     gridEl.classList.remove('gameOver');
 
+    const result = document.createElement('div');
+    result.classList.add('result');
+
     // generazione array con numero celle con bomba
 
-    let bombsCells = [];
-    while (bombsCells.length < 16) {
-        newBombCell = Math.floor(Math.random()*numOfCells) + 1;
-        if (!bombsCells.includes(newBombCell)){
-            bombsCells.push(newBombCell);
-        }
-    }
+    const numOfBombs = 16;
+    let bombsCells = getBombsArray(numOfBombs, numOfCells);
 
     // generazione array di controllo vincita
 
-    let freeCells = [];
-    for (let n = 1 ; n <= numOfCells; n++) {
-            freeCells.push(n);
-        }
-    freeCells= freeCells.filter((n) => !bombsCells.includes(n));
-    console.log(freeCells)
+    let freeCells = getFreeCellsArray(numOfCells, bombsCells)
+
 
     // pulizia e generazione griglia
 
     gridEl.innerHTML = '';
-    const result = document.createElement('div');
-    result.classList.add('result');
 
     for (let numCell = 1; numCell < numOfCells + 1; numCell++) {
 
@@ -132,3 +108,27 @@ function grid(numOfCells, difficulty){
     }
 }
 
+
+
+
+
+
+function getBombsArray(bombs, cells) {
+    let array = [];
+    while (array.length < bombs) {
+        let newBombCell = Math.floor(Math.random()*cells) + 1;
+        if (!array.includes(newBombCell)){
+            array.push(newBombCell);
+        }
+    }
+    return array;
+}
+
+function getFreeCellsArray(numOfCells, bombsArray) {
+    array = [];
+    for (let n =1; n < numOfCells; n++) {
+        array.push(n);
+    }
+    array = array.filter((n) => !bombsArray.includes(n));
+    return array
+}
